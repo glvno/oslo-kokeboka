cc8f5e97508c65c3b2c7b83bd5843e973416b4b3
Michael Glaviano <michael.glaviano@gmail.com>
2022-02-22 19:00:43 -0600

    capitalize page file names

---
+++ src/pages/About.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'React';
+const About: FC = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+);
+
+export default About;
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,3 @@
+import { FC } from 'React';
+const Admin: FC = () => <h1>Admin</h1>;
+export default Admin;
