6253b3462b23f241cdb0e25ba1bef92e701bde15
Michael Glaviano <michael.glaviano@gmail.com>
2022-03-01 00:27:38 -0600

    start on about page styling

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,8 +1,141 @@
 import { FC } from 'react';
+import Page from '../ui/Page';
+import styled from '@emotion/styled';
+
+const GreenBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 222.07px;
+  top: 130px;
+
+  /* GREEN */
+
+  background: #40993e;
+  transform: rotate(3.62deg);
+`;
+
+const SalmonBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 237.23px;
+  top: 290.27px;
+
+  /* SALMON */
+
+  background: #ff8585;
+  transform: rotate(-134.87deg);
+`;
+
+const RedBar = styled.div`
+  /* Rectangle 47 */
+
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 167.59px;
+  top: 242.64px;
+
+  /* RED */
+
+  background: #d04c4c;
+  transform: rotate(105.35deg);
+`;
+
+const BlueBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 131.7px;
+  top: 175px;
+
+  /* BLUE */
+
+  background: #6594f0;
+  transform: rotate(6deg);
+`;
+const WhiteBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 58px;
+  top: 284.06px;
+
+  /* PAPER */
+
+  background: #e9dfd6;
+  transform: rotate(-20.13deg);
+`;
+const PurpleBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 185.53px;
+  top: 266px;
+
+  /* PURPLE */
+
+  background: #785997;
+  transform: rotate(35.81deg);
+`;
+const PaperBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 58px;
+  top: 284.06px;
+
+  /* PAPER */
+
+  background: #e9dfd6;
+  transform: rotate(-20.13deg);
+`;
+const OchreBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 200px;
+  top: 318.21px;
+
+  /* OCHRE */
+
+  background: #f5bf57;
+  transform: rotate(-60.22deg);
+`;
+const PinkBar = styled.div`
+  position: absolute;
+  width: 6px;
+  height: 112px;
+  left: 245.15px;
+  top: 383px;
+
+  /* PINK */
+
+  background: #ebbfcf;
+  transform: rotate(82.96deg);
+`;
 const About: FC = () => (
-  <div>
-    <h1>About</h1>
-  </div>
+  <Page title="About">
+    <div>
+      <GreenBar />
+      <SalmonBar />
+      <RedBar />
+      <BlueBar />
+      <WhiteBar />
+      <PurpleBar />
+      <PaperBar />
+      <OchreBar />
+      <PinkBar />
+    </div>
+    <div>
+      Oslo is a small but vibrant, multicultural city, constantly changing, just like the food we
+      eat. Culture is never stagnant and neither are our recipes. In order to move past the complex
+      and ever excluding question of ‘what is Norwegian food.’ I am asking, 'what is Oslo’s food?’
+      To find that answer, I want to know what is the recipe that you connect to most and what are
+      the memories that meal brings forth?
+    </div>
+  </Page>
 );
 
 export default About;
