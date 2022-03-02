79f8e8ed04b53807cb0380c9817278334ce6c0df
Michael Glaviano <michael.glaviano@gmail.com>
2022-03-01 18:14:31 -0600

    fix: move About text into semantic element

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -123,7 +123,7 @@ const StyledDiv = styled.div`
   top: -160px;
   left: 0px;
 `;
-const StyledAbout = styled.div`
+const StyledAbout = styled.main`
   padding-top: 350px;
 `;
 const About: FC = () => {
