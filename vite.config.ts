c923b84ae918917c6c3b3d138182707222d578b3
Michael Glaviano <michael.glaviano@gmail.com>
2022-03-07 00:36:57 -0600

    wip:netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/oslo-kokebook/',
+  base: '/',
   server: {
     port: 3002,
     proxy: { '/recipe': 'http://localhost:3000/' },
